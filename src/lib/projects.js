11e306ec3349a59d926e19f418e1af2f6119e5e3
joaovictorcalaca <joaovictormcalaca@gmail.com>
2026-03-24 07:12:19 -0300

    changing id's

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -9,7 +9,7 @@ export const projects = [
     stacks: 'Next.js, Tailwind CSS'
   },
   {
-    id: 3,
+    id: 2,
     title: 'Movie Discover',
     description: 'Site simples para buscar filmes, consumindo a api do TMDB.',
     longDescription: 'Movie Discover é um site intuitivo que permite aos usuários buscar e descobrir filmes de maneira fácil e rápida. Utilizando a API do TMDB (The Movie Database), o site oferece uma vasta coleção de títulos, gêneros e informações detalhadas sobre cada filme. Os usuários podem pesquisar por nome, explorar categorias populares e obter recomendações personalizadas, tornando a experiência de encontrar novos filmes agradável e eficiente.',
@@ -18,7 +18,7 @@ export const projects = [
     stacks: 'Next.js, Tailwind CSS'
   },
   {
-    id: 5,
+    id: 3,
     title: 'Simulador de consumo de água',
     description: 'Aplicação que fornece dicas para economizar água geradas por IA.',
     longDescription: 'O Simulador de Consumo de Água é uma aplicação inovadora que utiliza inteligência artificial para ajudar os usuários a entender e reduzir seu consumo de água. Através de um simulador interativo, os usuários podem inserir dados sobre seus hábitos diários e receber dicas personalizadas para economizar água em casa. A IA analisa os padrões de uso e sugere mudanças simples que podem fazer uma grande diferença na conservação desse recurso vital.',
@@ -27,7 +27,7 @@ export const projects = [
     stacks: 'Next.js, Tailwind CSS, OpenAI API'
   },
   {
-    id: 6,
+    id: 4,
     title: 'jnews',
     description: 'Ainda em desenvolvimento, um agregador de notícias que consome a API do NewsAPI.',
     longDescription: 'jnews é um agregador de notícias em desenvolvimento que visa fornecer aos usuários uma plataforma centralizada para acessar as últimas notícias de diversas fontes. Utilizando a API do NewsAPI, o jnews permitirá que os usuários personalizem suas preferências de notícias, selecionando tópicos e fontes específicas. O objetivo é criar uma experiência de leitura de notícias eficiente e agradável, mantendo os usuários informados sobre os eventos atuais de maneira rápida e conveniente.',
